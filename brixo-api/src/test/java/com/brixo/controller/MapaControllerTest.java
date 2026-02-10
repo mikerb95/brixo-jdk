b40dfcfd3267286503fe0417ce1e883d94c65533
Mike <69970540+mikerb95@users.noreply.github.com>
2026-02-09 19:04:40 -0500

    fix: Update MapaControllerTest to use MockitoBean instead of MockBean

--- brixo-api/src/test/java/com/brixo/controller/MapaControllerTest.java
+++ brixo-api/src/test/java/com/brixo/controller/MapaControllerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.bean.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -25,7 +25,7 @@ class MapaControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private MapaService mapaService;
 
     @Test
